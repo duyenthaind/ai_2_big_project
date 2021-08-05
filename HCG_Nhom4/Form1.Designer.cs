651d26391f108d544fbad472166a24bb2758cc3c
duyenthaind <duyenthai123456@gmail.com>
2021-08-05 23:16:42 +0700

    feature - update lable group

--- HCG_Nhom4/Form1.Designer.cs
+++ HCG_Nhom4/Form1.Designer.cs
@@ -49,83 +49,83 @@ namespace HCG_Nhom4
             this.panel2.BackColor = System.Drawing.SystemColors.Highlight;
             this.panel2.Controls.Add(this.lbltitle);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(417, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel2.Location = new System.Drawing.Point(208, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1758, 156);
+            this.panel2.Size = new System.Drawing.Size(754, 81);
             this.panel2.TabIndex = 5;
             // 
             // lbltitle
             // 
             this.lbltitle.AutoSize = true;
-            this.lbltitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.lbltitle.ForeColor = System.Drawing.Color.Gainsboro;
-            this.lbltitle.Location = new System.Drawing.Point(814, 53);
-            this.lbltitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbltitle.Location = new System.Drawing.Point(407, 28);
+            this.lbltitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbltitle.Name = "lbltitle";
-            this.lbltitle.Size = new System.Drawing.Size(239, 73);
+            this.lbltitle.Size = new System.Drawing.Size(122, 37);
             this.lbltitle.TabIndex = 0;
             this.lbltitle.Text = "Tư Vấn";
             // 
             // panelview
             // 
             this.panelview.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelview.Location = new System.Drawing.Point(417, 0);
-            this.panelview.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panelview.Location = new System.Drawing.Point(208, 0);
+            this.panelview.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.panelview.Name = "panelview";
-            this.panelview.Padding = new System.Windows.Forms.Padding(30, 78, 0, 0);
-            this.panelview.Size = new System.Drawing.Size(1758, 1399);
+            this.panelview.Padding = new System.Windows.Forms.Padding(15, 41, 0, 0);
+            this.panelview.Size = new System.Drawing.Size(754, 552);
             this.panelview.TabIndex = 4;
             // 
             // panel3
             // 
-            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(74)))));
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (40)))), ((int) (((byte) (40)))), ((int) (((byte) (74)))));
             this.panel3.Controls.Add(this.label1);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.Location = new System.Drawing.Point(0, 0);
             this.panel3.Margin = new System.Windows.Forms.Padding(0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(417, 156);
+            this.panel3.Size = new System.Drawing.Size(208, 81);
             this.panel3.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label1.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label1.Location = new System.Drawing.Point(99, 53);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(50, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(239, 61);
+            this.label1.Size = new System.Drawing.Size(108, 31);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Nhóm 10";
+            this.label1.Text = "Nhóm 4";
             // 
             // panelmenu
             // 
-            this.panelmenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.panelmenu.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (51)))), ((int) (((byte) (51)))), ((int) (((byte) (76)))));
             this.panelmenu.Controls.Add(this.panel3);
             this.panelmenu.Controls.Add(this.btntuvan);
             this.panelmenu.Controls.Add(this.btnsukien);
             this.panelmenu.Controls.Add(this.btntapluat);
             this.panelmenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelmenu.Location = new System.Drawing.Point(0, 0);
-            this.panelmenu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panelmenu.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.panelmenu.Name = "panelmenu";
-            this.panelmenu.Size = new System.Drawing.Size(417, 1399);
+            this.panelmenu.Size = new System.Drawing.Size(208, 552);
             this.panelmenu.TabIndex = 3;
             // 
             // btntuvan
             // 
             this.btntuvan.FlatAppearance.BorderSize = 0;
             this.btntuvan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btntuvan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntuvan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.btntuvan.ForeColor = System.Drawing.Color.Gainsboro;
-            this.btntuvan.Image = ((System.Drawing.Image)(resources.GetObject("btntuvan.Image")));
+            this.btntuvan.Image = ((System.Drawing.Image) (resources.GetObject("btntuvan.Image")));
             this.btntuvan.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btntuvan.Location = new System.Drawing.Point(0, 156);
+            this.btntuvan.Location = new System.Drawing.Point(0, 81);
             this.btntuvan.Margin = new System.Windows.Forms.Padding(0);
             this.btntuvan.Name = "btntuvan";
-            this.btntuvan.Size = new System.Drawing.Size(417, 156);
+            this.btntuvan.Size = new System.Drawing.Size(208, 81);
             this.btntuvan.TabIndex = 1;
             this.btntuvan.Text = "Tư Vấn";
             this.btntuvan.UseVisualStyleBackColor = true;
@@ -135,14 +135,14 @@ namespace HCG_Nhom4
             // 
             this.btnsukien.FlatAppearance.BorderSize = 0;
             this.btnsukien.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnsukien.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsukien.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.btnsukien.ForeColor = System.Drawing.Color.Gainsboro;
-            this.btnsukien.Image = ((System.Drawing.Image)(resources.GetObject("btnsukien.Image")));
+            this.btnsukien.Image = ((System.Drawing.Image) (resources.GetObject("btnsukien.Image")));
             this.btnsukien.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnsukien.Location = new System.Drawing.Point(0, 312);
+            this.btnsukien.Location = new System.Drawing.Point(0, 162);
             this.btnsukien.Margin = new System.Windows.Forms.Padding(0);
             this.btnsukien.Name = "btnsukien";
-            this.btnsukien.Size = new System.Drawing.Size(417, 156);
+            this.btnsukien.Size = new System.Drawing.Size(208, 81);
             this.btnsukien.TabIndex = 2;
             this.btnsukien.Text = "Sự Kiện";
             this.btnsukien.UseVisualStyleBackColor = true;
@@ -152,14 +152,14 @@ namespace HCG_Nhom4
             // 
             this.btntapluat.FlatAppearance.BorderSize = 0;
             this.btntapluat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btntapluat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntapluat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.btntapluat.ForeColor = System.Drawing.Color.Gainsboro;
-            this.btntapluat.Image = ((System.Drawing.Image)(resources.GetObject("btntapluat.Image")));
+            this.btntapluat.Image = ((System.Drawing.Image) (resources.GetObject("btntapluat.Image")));
             this.btntapluat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btntapluat.Location = new System.Drawing.Point(0, 468);
+            this.btntapluat.Location = new System.Drawing.Point(0, 243);
             this.btntapluat.Margin = new System.Windows.Forms.Padding(0);
             this.btntapluat.Name = "btntapluat";
-            this.btntapluat.Size = new System.Drawing.Size(417, 156);
+            this.btntapluat.Size = new System.Drawing.Size(208, 81);
             this.btntapluat.TabIndex = 3;
             this.btntapluat.Text = "Tập Luật";
             this.btntapluat.UseVisualStyleBackColor = true;
@@ -167,12 +167,13 @@ namespace HCG_Nhom4
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2175, 1399);
+            this.ClientSize = new System.Drawing.Size(962, 552);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panelview);
             this.Controls.Add(this.panelmenu);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -182,7 +183,6 @@ namespace HCG_Nhom4
             this.panel3.PerformLayout();
             this.panelmenu.ResumeLayout(false);
             this.ResumeLayout(false);
-
         }
 
         #endregion
